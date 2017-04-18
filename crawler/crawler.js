2f315c2859a01d591bc56dbb72c09f740ac0be66
panfen <2809956575@qq.com>
2017-04-18 21:44:51 +0800

    'update'

--- crawler/crawler.js
+++ crawler/crawler.js
@@ -1,53 +1,60 @@
 'use strict'
 
-var request = require('request');
+var co = require('co');
 var cheerio = require('cheerio');
 var Promise = require('bluebird');
+var request = Promise.promisify(require('request'));
 
 var URL = 'http://www.piaohua.com/';
 
 function getftpLink(link){
-	request.get(link,function(err,res,body){
-		if(!err && res.statusCode == 200){
-			var $ = cheerio.load(body);
-			var ftp = $('#showinfo').find('table tbody tr td a').html();
-			return ftp;
-		}else{
-			return 'failed to get the ftp!';
-		}
-	});
-}
-
-exports.getMovieList = function(){
 	return new Promise(function(resolve,reject){
-		request.get(URL,function(err,res,body){
+		request.get(link,function(err,res,body){
 			if(!err && res.statusCode == 200){
 				var $ = cheerio.load(body);
-				var movieList = $('#iml1').children("ul").first().find('li');
-				var myMovieList = [];
-
-				movieList.each(function(item){
-					var time = $(this).find('span font').html() ? $(this).find('span font').html() : $(this).find('span').html();
-					if((new Date() - new Date(time)) < 259200000){
-						var dom = $(this).find('a').first();
-						var link = URL + $(dom).attr('href');
-						var img = $(dom).find('img').attr('src');
-						var name = $(dom).find('img').attr('alt').substr(22).replace('</font>','');
-						var ftp = getftpLink(link);
-						var movie = {
-							name:name,
-							img:img,
-							link:link,
-							time:time,
-							ftp:unescape(ftp.replace(/;/g,'').replace(/&#x/g, "%u"))
-						}
-						myMovieList.push(movie);			
-					};
-				});
-				resolve(myMovieList);
+				var ftp = $('#showinfo').find('table tbody tr td a').html();
+				resolve(ftp);
 			}else{
-				reject('failed to crawl the web!');
+				reject('failed to get the ftp!');
 			}
 		});
 	});
 }
+
+function getPageByUrl(){
+	return new Promise(function(resolve,reject){
+		request.get(URL,function(err,res,body){
+			var $ = cheerio.load(body);
+			var _movieList = [];
+			var movieList = $('#iml1').children("ul").first().find('li');
+			movieList.each(function(item){
+				var time = $(this).find('span font').html() ? $(this).find('span font').html() : $(this).find('span').html();
+				if((new Date() - new Date(time)) < 259200000){
+					var dom = $(this).find('a').first();
+					var link = URL + $(dom).attr('href');
+					var img = $(dom).find('img').attr('src');
+					var name = $(dom).find('img').attr('alt').substr(22).replace('</font>','');
+					var movie = {
+						name:name,
+						img:img,
+						link:link,
+						time:time,
+					}
+					_movieList.push(movie);		
+				};
+			});
+			resolve(_movieList);
+		});
+	});
+}
+
+exports.getMovieList = co(function *fectchData() {
+	var movieList = []
+	var _movieList = yield getPageByUrl();
+	for(var i=0;i<_movieList.length;i++){
+		var ftp = yield getftpLink(_movieList[i].link);
+		_movieList[i].ftp = unescape(ftp.replace(/;/g,'').replace(/&#x/g, "%u"));
+		movieList.push(_movieList[i]);
+	}
+	return movieList
+});
